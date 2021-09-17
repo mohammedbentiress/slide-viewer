18f4415e1b86df6535dcd5c2ce840323e5238a38
p2112390 <ikhadiri@sqli.com>
2021-09-17 10:09:55 +0200

    edit index.tsx file

--- client/components/Content/index.jsx
+++ client/components/Content/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import LOGO from '../images/react.png'
 
-export default function Content() {
+export default function Content () {
   return <img src={LOGO} alt='Logo'/>
 }
